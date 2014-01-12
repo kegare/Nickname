fba46cc1db88c979913f312f6e17f07824ef6853
kegare <kegare0127@gmail.com>
2014-01-12 15:55:49 +0900

    Changes for 1.0.1 build.

--- kegare/nickname/Nickname.java
+++ kegare/nickname/Nickname.java
@@ -1,18 +1,23 @@
 package kegare.nickname;
 
+import com.google.common.base.Strings;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
-import com.google.common.base.Strings;
-
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
 
 @Mod
 (
@@ -25,6 +30,24 @@ import cpw.mods.fml.common.network.NetworkMod;
 )
 public class Nickname
 {
+	protected static final Properties nicknameTable = new Properties();
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		try
+		{
+			File file = new File("nickname.xml");
+
+			if (file.exists())
+			{
+				nicknameTable.clear();
+				nicknameTable.loadFromXML(new FileInputStream(file));
+			}
+		}
+		catch (Exception ignored) {}
+	}
+
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
@@ -37,10 +60,46 @@ public class Nickname
 		String message = event.message;
 		EntityPlayerMP player = event.player;
 
-		if (!Strings.isNullOrEmpty(message) && message.startsWith("@"))
+		if (!Strings.isNullOrEmpty(message) && message.startsWith("@nickname"))
 		{
-			player.getEntityData().setString("Nickname", message.substring(1));
-			player.refreshDisplayName();
+			try
+			{
+				String username = player.username;
+				String nickname = message.substring(message.indexOf(' ') + 1);
+
+				if (Strings.isNullOrEmpty(nickname) || nickname.equalsIgnoreCase("@nickname"))
+				{
+					throw new NullPointerException();
+				}
+				else
+				{
+					if ("default".equalsIgnoreCase(nickname) || "username".equalsIgnoreCase(nickname))
+					{
+						nicknameTable.setProperty(username, username);
+
+						player.sendChatToPlayer(ChatMessageComponent.createFromText("Your nickname has been set to \"" + username + "\"").setColor(EnumChatFormatting.GRAY).setItalic(true));
+					}
+					else
+					{
+						nicknameTable.setProperty(username, nickname);
+
+						player.sendChatToPlayer(ChatMessageComponent.createFromText("Your nickname has been set to \"" + nickname + "\"").setColor(EnumChatFormatting.GRAY).setItalic(true));
+					}
+
+					if (!nicknameTable.isEmpty())
+					{
+						nicknameTable.storeToXML(new FileOutputStream(new File("nickname.xml")), null);
+					}
+
+					player.refreshDisplayName();
+				}
+			}
+			catch (Exception e)
+			{
+				player.sendChatToPlayer(ChatMessageComponent.createFromText("Usage: @nickname <[Nickname]|default>").setColor(EnumChatFormatting.DARK_GRAY));
+			}
+
+			event.setCanceled(true);
 		}
 	}
 
@@ -50,11 +109,11 @@ public class Nickname
 		if (event.entityPlayer instanceof EntityPlayerMP)
 		{
 			EntityPlayerMP player = (EntityPlayerMP)event.entityPlayer;
-			NBTTagCompound nbt = player.getEntityData();
+			String username = player.username;
 
-			if (nbt.hasKey("Nickname"))
+			if (nicknameTable.containsKey(username))
 			{
-				event.displayname = nbt.getString("Nickname");
+				event.displayname = nicknameTable.getProperty(username, username);
 			}
 		}
 	}
